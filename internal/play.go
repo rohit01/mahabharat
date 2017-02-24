d14b78475514e95fdd83833ae84a2bebdc8bcd89
rohit01 <hello@rohit.io>
2017-02-24 16:34:50 +0530

    Attack moves added

--- internal/play.go
+++ internal/play.go
@@ -3,6 +3,7 @@
 package internal
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"strconv"
@@ -11,12 +12,15 @@ import (
 
 type (
 	Player struct {
-		Name         string
+		PlayerNo     int
 		Hits         int
+		AttackMoves  []CoOrdinates
+		Winner       bool
+		Turn         chan struct{}
 		battleGround []([]string)
 	}
 
-	Position struct {
+	CoOrdinates struct {
 		X int
 		Y int
 	}
@@ -33,15 +37,16 @@ func (self *Player) GroundStatus() string {
 	return groundStatus
 }
 
-func (self *Player) BombedAt(x, y int) (bool, error) {
-	if (x >= len(self.battleGround)) || (y >= len(self.battleGround)) {
-		return false, fmt.Errorf("Missile attacked (at %v,%v) beyond the grid length: %v", x, y, len(self.battleGround))
+func (self *Player) Bomb(position CoOrdinates) (bool, error) {
+	if (position.X >= len(self.battleGround)) || (position.Y >= len(self.battleGround)) {
+		return false, fmt.Errorf("Missile attacked (at %v,%v) beyond the grid length: %v",
+			position.X, position.Y, len(self.battleGround))
 	}
-	if (self.battleGround[x][y] == "B") || (self.battleGround[x][y] == "X") {
-		self.battleGround[x][y] = "X"
+	if (self.battleGround[position.X][position.Y] == "B") || (self.battleGround[position.X][position.Y] == "X") {
+		self.battleGround[position.X][position.Y] = "X"
 		return true, nil
 	} else {
-		self.battleGround[x][y] = "O"
+		self.battleGround[position.X][position.Y] = "O"
 	}
 	return false, nil
 }
@@ -57,12 +62,32 @@ func (self *Player) AllShipsDestroyed() bool {
 	return true
 }
 
-func New(name string, gridSize int, shipPositions string) (*Player, error) {
+func (self *Player) Attack(ctx context.Context, opponent *Player, gameOver chan struct{}) {
+	for _, position := range self.AttackMoves {
+		select {
+		case <-self.Turn:
+			opponent.Bomb(position)
+			if opponent.AllShipsDestroyed() {
+				self.Winner = true
+				gameOver <- struct{}{}
+			}
+			opponent.Turn <- struct{}{}
+		case <-ctx.Done():
+			fmt.Println("Context done")
+			gameOver <- struct{}{}
+		}
+	}
+	gameOver <- struct{}{}
+}
+
+func New(playerNo int, gridSize int, shipGridString, attackGridString string) (*Player, error) {
 	if gridSize <= 0 {
 		return nil, errors.New("Grid Size must be a positive integer")
 	}
 	// Allocate memory & initialize
 	player := new(Player)
+	player.Turn = make(chan struct{})
+	player.PlayerNo = playerNo
 	player.battleGround = make([]([]string), gridSize)
 	for rowNumber := 0; rowNumber < gridSize; rowNumber++ {
 		player.battleGround[rowNumber] = make([]string, gridSize)
@@ -70,19 +95,24 @@ func New(name string, gridSize int, shipPositions string) (*Player, error) {
 			player.battleGround[rowNumber][columnNumber] = "_"
 		}
 	}
-	// Process input positions
-	initialPositions, err := ParseGridString(shipPositions)
+	// Process input positions & set attack moves
+	initialPositions, err := ParseGridString(shipGridString)
 	if err != nil {
 		return nil, err
 	}
 	for _, position := range initialPositions {
 		player.battleGround[position.X][position.Y] = "B"
 	}
+	attackMoves, err := ParseGridString(attackGridString)
+	if err != nil {
+		return nil, err
+	}
+	player.AttackMoves = attackMoves
 	return player, nil
 }
 
-func ParseGridString(gridString string) (moves []Position, err error) {
-	moves = make([]Position, 0)
+func ParseGridString(gridString string) (moves []CoOrdinates, err error) {
+	moves = make([]CoOrdinates, 0)
 	for _, position := range strings.Split(gridString, ",") {
 		xyPosition := strings.Split(position, ":")
 		x, err := strconv.ParseInt(xyPosition[0], 10, 64)
@@ -93,7 +123,7 @@ func ParseGridString(gridString string) (moves []Position, err error) {
 		if err != nil {
 			return nil, fmt.Errorf("Invalid grid position data: %v", gridString)
 		}
-		moves = append(moves, Position{int(x), int(y)})
+		moves = append(moves, CoOrdinates{int(x), int(y)})
 	}
 	return moves, nil
 }
